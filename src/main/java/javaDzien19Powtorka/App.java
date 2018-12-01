1927b5ad6bc78ecf3bd6a3237081783dcc41b17a
Michal Skrzypczak <michalskrzypczak91@gmail.com>
2018-12-01 09:17:26 +0100

    Initial commit

---
+++ src/main/java/javaDzien19Powtorka/App.java
@@ -0,0 +1,13 @@
+package javaDzien19Powtorka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
